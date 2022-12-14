b0cb9d6fbe0486b3ef8f6e1b9d94f90e8cc6cac8
djamel1443 <dalmabouada@ivytech.edu>
2022-12-14 13:17:03 -0500

    Added a new html to post list

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,9 @@
-# Create your models here.
 
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -12,6 +11,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+# methods or functions for this post
     def publish(self):
         self.published_date = timezone.now()
         self.save()
